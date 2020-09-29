d568f39204bcb23efa94c8d7fdad6332eb6ee9b9
Dan <igaryhe@gmail.com>
2020-09-29 14:57:29 +0800

    init

---
+++ Assets/Scripts/CustomImageEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+ 
+[ExecuteInEditMode]
+public class CustomImageEffect : MonoBehaviour {
+ 
+    public Material material;
+
+    private void Awake()
+    {
+        gameObject.GetComponent<Camera>().depthTextureMode = DepthTextureMode.DepthNormals;
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        Graphics.Blit(src, dest, material);
+    }
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float start;
+    private float current = 0;
+    private TextMeshProUGUI _text;
+
+    private void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        _text.text = "00:00";
+        current = start;
+    }
+
+    private void Update()
+    {
+        if (current <= 0) current = 0;
+        current -= 1 * Time.deltaTime;
+        var min = (int) (current / 60f);
+        var sec = (int) (current % 60f);
+        _text.text = min.ToString("00") + ":" + sec.ToString("00");
+    }
+}

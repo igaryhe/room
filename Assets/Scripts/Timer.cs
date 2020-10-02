270155ef96e3a69c908be48fe854d2d620abf52a
Dan <igaryhe@gmail.com>
2020-10-02 17:52:42 +0800

    timer & sfx

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,25 +1,34 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    public float start;
-    private float current = 0;
+    public int start;
+    public AudioClip tick;
+    private AudioSource _audioSource;
+    private int current = 0;
     private TextMeshProUGUI _text;
 
     private void Start()
     {
+        _audioSource = gameObject.AddComponent<AudioSource>();
         _text = GetComponent<TextMeshProUGUI>();
         _text.text = "00:00";
         current = start;
+        StartCoroutine(Tick());
     }
-
-    private void Update()
+    
+    private IEnumerator Tick()
     {
-        if (current <= 0) current = 0;
-        current -= 1 * Time.deltaTime;
-        var min = (int) (current / 60f);
-        var sec = (int) (current % 60f);
-        _text.text = min.ToString("00") + ":" + sec.ToString("00");
+        while (current >= 0)
+        {
+            _audioSource.PlayOneShot(tick, 1f);
+            var min = current / 60;
+            var sec = current % 60;
+            _text.text = min.ToString("00") + ":" + sec.ToString("00");
+            current--;
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
